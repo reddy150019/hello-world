ca490af05ed5bef846fdad66e8f8fd3785f718fb
reddy150019 <reddy150019@gmail.com>
2021-12-23 06:44:27 +0000

    added new

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,7 +4,5 @@ public class HelloWorld {
     System.out.println("Hello, nagesh");
     System.out.println("Hello, hari");
     System.out.println("Hello, siva");
-    System.out.println("Hello, malli");
-    System.out.println("Hello, World");
   }
 }
