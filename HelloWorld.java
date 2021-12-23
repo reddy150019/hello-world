c6080e2906557dbfa45d2359564b2852c83c4444
reddy150019 <reddy150019@gmail.com>
2021-12-23 06:58:39 +0000

    added new linesss3

--- HelloWorld.java
+++ HelloWorld.java
@@ -7,10 +7,5 @@ public class HelloWorld {
     System.out.println("Hello, World");
     System.out.println("Hello, World");
     System.out.println("Hello, World");
-    System.out.println("Hello, World");
-    System.out.println("Hello, World");
-    System.out.println("Hello, World");
-    System.out.println("Hello, World");
-    System.out.println("Hello, World");
   }
 }
