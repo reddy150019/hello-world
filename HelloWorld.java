3279e84e2cb71223c9eed0df12929952f0d089c1
reddy150019 <reddy150019@gmail.com>
2021-12-23 06:37:55 +0000

    added new matter

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,10 +1,10 @@
 public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello, World");
-    System.out.println("Hello, World");
-    System.out.println("Hello, World");
-    System.out.println("Hello, World");
-    System.out.println("Hello, World");
+    System.out.println("Hello, nagesh");
+    System.out.println("Hello, hari");
+    System.out.println("Hello, siva");
+    System.out.println("Hello, malli");
     System.out.println("Hello, World");
   }
 }
