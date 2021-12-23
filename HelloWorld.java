14716e2fffb08d7bb1ea0511d37179da6f460908
reddy150019 <reddy150019@gmail.com>
2021-12-23 07:00:54 +0000

    added new linesss7

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,5 @@
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("Hello, World");
     System.out.println("Hello, nagesh");
     System.out.println("Hello, hari");
     System.out.println("Hello, siva");
